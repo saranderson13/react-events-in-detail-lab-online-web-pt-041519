c7aa3475410b6375708bb2b09f6e47db83bdf4a2
Sarah <saranderson13@gmail.com>
2020-01-25 17:45:11 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 export default class CoordinatesButton extends Component {
 
-    constructor(props) {
-        super()
-    }
+    // constructor() {
+    //     super()
+    // }
 
     render() {
         return (
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 export default class DelayedButton extends Component {
 
-    constructor(props) {
-        super()
-    }
+    // constructor() {
+    //     super()
+    // }
 
     render() {
-        return <button onClick={ (e) => {
-            e.persist()
-            this.props.onDelayedClick(e)
-        }}>Delayed</button>;
+        return (
+            <button onClick={ (e) => {
+                e.persist()
+                this.props.onDelayedClick(e)
+            }}>Delayed</button>
+        )
     }
 
 }
